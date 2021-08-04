ca18fe1f5011c425355faa22b62c9a976776c881
nik92eth <67508712+nik92eth@users.noreply.github.com>
2021-08-04 18:54:22 +0530

    Add files via upload

---
+++ node/app.py
@@ -0,0 +1,186 @@
+""" Local Node Server """
+from os import getenv, path, makedirs
+import sys
+import shutil
+import logging
+from random import randrange
+from flask import Flask, jsonify
+from flask_cors import CORS
+import requests
+import ipfshttpclient
+from web3 import Web3, HTTPProvider
+from apscheduler.schedulers.background import BackgroundScheduler
+from dotenv import load_dotenv
+import tensorflow as tf
+from contract import contractABI, contractAddress
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]iasdfjfsd/'
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
+
+ipfs_api = '/dns/ipfs.infura.io/tcp/5001/https'
+client = ipfshttpclient.connect(ipfs_api)
+print(f"Connected to IPFS v{client.version()['Version']}")
+
+active_tasks = []
+global sentinel_contract
+
+port = int(getenv('PORT', str(5005)))
+
+def add_to_ipfs(fn="model.h5"):
+
+  """ Add File to IPFS """
+
+  res = client.add(fn)
+  return res['Hash']
+
+
+def add_self_to_exchange():
+
+  """ Add Server to Exchange """
+
+  params = {'eth_address': getenv('ETHADDRESS'), 'ip': getenv('NODE1_URL')}
+  posturl = f"{getenv('COORDINATOR_URL')}{getenv('NODES_ENDPOINT')}"
+  try:
+    resp = requests.post(posturl, json=params)
+  except:
+    print("Coordinator Node is Offline")
+    sys.exit(0)
+
+  if resp.status_code != 200:
+    print("Coordinator Node is Offline")
+    sys.exit(0)
+  else:
+    print("Connected to Coordinator Node")
+
+
+@app.route('/')
+def server():
+
+  """ Online Test """
+
+  html = """<p style='font-family: monospace;padding: 10px;'>
+            Local Training Node is online 🚀 </p>"""
+
+  return html
+
+
+@app.route('/status')
+def status():
+
+  """ Returns server status """
+
+  server_data = {
+      "active_tasks": active_tasks
+  }
+
+  return jsonify(server_data), 200
+
+
+@app.route('/start-training/', defaults={'task_id': 0}, methods=['GET', 'POST'])
+@app.route('/start-training/<int:task_id>', methods=['GET', 'POST'])
+def start_training(task_id):
+
+  """ Training Start """
+
+  if task_id < 1:
+    return jsonify("Invalid Task"), 400
+  else:
+    active_tasks.append(task_id)
+    return jsonify("Task Added"), 200
+
+
+def background_trainer():
+
+  """ Background Transaction Handler """
+
+  for ind, val in enumerate(active_tasks):
+
+    print(f"TRAINING MODEL FOR TASK {val}")
+    model_hashes = sentinel_contract.functions.getTaskHashes(val).call()
+    model_hashes = [x for x in list(model_hashes) if x.strip()]
+    task_data = sentinel_contract.functions.SentinelTasks(val).call() # taskID, currentRound, totalRounds, cost
+
+    if len(model_hashes) >= task_data[2]:
+      print(f"TASKID:{val} is completed.")
+    else:
+      try:
+        if len(model_hashes) >= 1:
+          active_hash = model_hashes[-1]
+          print(f"Fetching {active_hash} from IPFS")
+          if not path.exists(f'model_storage/{active_hash}'):
+            client.get(active_hash)
+            shutil.move(active_hash, 'model_storage')
+          new_model = tf.keras.models.load_model(f"model_storage/{active_hash}")
+          print(new_model.summary())
+          print("Now Training Model")
+          new_model.fit(xtrain, ytrain, epochs=1, verbose=0)
+          fn = f"model_storage/{randrange(1000, 99999)}.h5"
+          new_model.save(fn)
+          new_hash = add_to_ipfs(fn)
+          shutil.move(fn, f'model_storage/{new_hash}')
+          print(f"TASKID:{val} Trained {new_hash}")
+
+          params = {
+              'ethAddress': getenv('ETHADDRESS'),
+              'modelHash': new_hash
+          }
+          url = f"{getenv('COORDINATOR_URL')}{getenv('NEXTRUN_ENDPOINT')}/{val}"
+          try:
+            requests.post(url, json=params)
+          except:
+            print(f"Coordinator Node is Offline while POSTING RESULT")
+            print(params)
+
+        else:
+          print("Invalid Task")
+
+      except:
+        print("Error in Training Model")
+
+    active_tasks.pop(ind)
+
+# Start Initialization
+
+w3 = Web3(HTTPProvider('https://betav2.matic.network'))
+if not w3.isConnected():
+  print("Web3 Not Connected")
+  sys.exit(0)
+else:
+  print(f'Connected to Web3 v{w3.api}')
+
+add_self_to_exchange()
+
+sentinel_contract = w3.eth.contract(address=contractAddress, abi=contractABI)
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(background_trainer, "interval", seconds=10)
+sched.start()
+
+if not path.exists('model_storage'): makedirs('model_storage')
+
+mnist = tf.keras.datasets.mnist
+(xtrain, ytrain), (xtest, ytest) = mnist.load_data()
+xtrain = tf.keras.utils.normalize(xtrain, axis=1)
+xtest = tf.keras.utils.normalize(xtest, axis=1)
+print("Data Loaded")
+
+# End Initialization
+
+if __name__ != "__main__":
+
+  gunicorn_logger = logging.getLogger('gunicorn.error')
+  app.logger.handlers = gunicorn_logger.handlers
+  app.logger.setLevel(gunicorn_logger.level)
+
+if __name__ == '__main__':
+
+  app.run(
+      host="0.0.0.0",
+      port=int(getenv('PORT', str(5005))),
+      debug=False,
+      use_reloader=False,
+      threaded=True)
---
+++ node/contract.py
@@ -0,0 +1,224 @@
+
+contractABI= [
+	{
+		"constant": False,
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_modelHash",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_rounds",
+				"type": "uint256"
+			}
+		],
+		"name": "createTask",
+		"outputs": [],
+		"payable": True,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"anonymous": False,
+		"inputs": [
+			{
+				"indexed": True,
+				"internalType": "uint256",
+				"name": "taskID",
+				"type": "uint256"
+			},
+			{
+				"indexed": False,
+				"internalType": "string",
+				"name": "_modelHash",
+				"type": "string"
+			},
+			{
+				"indexed": False,
+				"internalType": "uint256",
+				"name": "_time",
+				"type": "uint256"
+			}
+		],
+		"name": "modelUpdated",
+		"type": "event"
+	},
+	{
+		"anonymous": False,
+		"inputs": [
+			{
+				"indexed": True,
+				"internalType": "uint256",
+				"name": "taskID",
+				"type": "uint256"
+			},
+			{
+				"indexed": True,
+				"internalType": "address",
+				"name": "_user",
+				"type": "address"
+			},
+			{
+				"indexed": False,
+				"internalType": "string",
+				"name": "_modelHash",
+				"type": "string"
+			},
+			{
+				"indexed": False,
+				"internalType": "uint256",
+				"name": "_amt",
+				"type": "uint256"
+			},
+			{
+				"indexed": False,
+				"internalType": "uint256",
+				"name": "_time",
+				"type": "uint256"
+			}
+		],
+		"name": "newTaskCreated",
+		"type": "event"
+	},
+	{
+		"constant": False,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_taskID",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "_modelHash",
+				"type": "string"
+			},
+			{
+				"internalType": "address payable",
+				"name": "computer",
+				"type": "address"
+			}
+		],
+		"name": "updateModelForTask",
+		"outputs": [],
+		"payable": False,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": True,
+		"inputs": [],
+		"name": "getTaskCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": False,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": True,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_taskID",
+				"type": "uint256"
+			}
+		],
+		"name": "getTaskHashes",
+		"outputs": [
+			{
+				"internalType": "string[]",
+				"name": "",
+				"type": "string[]"
+			}
+		],
+		"payable": False,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": True,
+		"inputs": [],
+		"name": "getTasksOfUser",
+		"outputs": [
+			{
+				"internalType": "uint256[]",
+				"name": "",
+				"type": "uint256[]"
+			}
+		],
+		"payable": False,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": True,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "SentinelTasks",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "taskID",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "currentRound",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "totalRounds",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "cost",
+				"type": "uint256"
+			}
+		],
+		"payable": False,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": True,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "UserTaskIDs",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": False,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+contractAddress = "0xA7F599AeE2dd3AB3a29D54820a9Ee12938cdb2fC"
